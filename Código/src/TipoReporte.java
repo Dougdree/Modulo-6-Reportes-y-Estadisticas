c0f7fe9385e6bd465657c9c5fa421868c8923754
Dougdree <douglas.carreno@unl.edu.ec>
2024-11-26 04:11:49 -0500

    Delete Código directory
    borrare esto para subir el nuevo codigo corregido

--- Código/src/TipoReporte.java
+++
@@ -1,6 +0,0 @@
-// Enumeración para definir los tipos de reportes generados.
-public enum TipoReporte {
-    DIARIO,      // Reporte generado diariamente.
-    SEMANAL,     // Reporte generado semanalmente.
-    MENSUAL      // Reporte generado mensualmente.
-}
