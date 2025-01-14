abbc5ba85c68bf6dc7a5df1fcbc10aeb1e55aa14
vivianaCordova <viviana.e.cordova@unl.edu.ec>
2025-01-14 10:57:03 -0500

    Update admin.py

--- MODULO6/Estadisticas/admin.py
+++ MODULO6/Estadisticas/admin.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
+
+from .models import (
+    Producto, Item_Factura, Factura, Mesa, Persona, Mesero,estadistica_mesero, estadistica_mesa, estadistica_producto, Reporte, Grafico
+)
+
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio', 'categoria',)
@@ -12,13 +18,13 @@ class Item_FacturaAdmin(admin.ModelAdmin):
 
 @admin.register(Factura)
 class FacturaAdmin(admin.ModelAdmin):
-    list_display = ('numero', 'fecha', )
+    list_display = ('numero', 'fecha', 'total', )
     search_fields = ('numero', 'fecha',)
     list_filter = ('numero', 'fecha', )
 
 @admin.register(Mesa)
 class MesaAdmin(admin.ModelAdmin):
-    list_display = ('cantidad_uso', 'codigo',)
+    list_display = ('cantidad_uso', 'codigo', )
     search_fields = ('cantidad_uso', 'codigo',)
     list_filter = ('cantidad_uso', 'codigo',)
 
@@ -30,33 +36,33 @@ class PersonaAdmin(admin.ModelAdmin):
 
 @admin.register(Mesero)
 class MeseroAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'cedula',)
-    search_fields = ('nombre', 'cedula',)
+    list_display = ('nombre', 'cedula', 'pedidosAtendidos',)
+    search_fields = ('nombre', 'cedula','pedidosAtendidos',)
     list_filter = ('nombre', 'cedula',)
 
 @admin.register(estadistica_mesero)
 class estadistica_meseroAdmin(admin.ModelAdmin):
-    list_display = ('mejor_mesero',)
-    search_fields = ('mejor_mesero',)
-    list_filter = ('mejor_mesero',)
+    list_display = ('titulo', 'mejor_mesero',)
+    search_fields = ('titulo', 'mejor_mesero',)
+    list_filter = ('titulo', 'mejor_mesero',)
 
 @admin.register(estadistica_mesa)
 class estadistica_mesaAdmin(admin.ModelAdmin):
-    list_display = ('mesa_mas_usada',)
-    search_fields = ('mesa_mas_usada',)
-    list_filter = ('mesa_mas_usada',)
+    list_display = ('titulo', 'mesa_mas_usada',)
+    search_fields = ('titulo', 'mesa_mas_usada',)
+    list_filter = ('titulo', 'mesa_mas_usada',)
 
 @admin.register(estadistica_producto)
 class estadistica_productoAdmin(admin.ModelAdmin):
-    list_display = ('producto_mas_vendido',)
-    search_fields = ('producto_mas_vendido',)
-    list_filter = ('producto_mas_vendido',)
+    list_display = ('titulo', 'producto_mas_vendido',)
+    search_fields = ('titulo', 'producto_mas_vendido',)
+    list_filter = ('titulo', 'producto_mas_vendido',)
 
 @admin.register(Reporte)
 class ReporteAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'estadistica_list',)
-    search_fields = ('titulo', 'estadistica_list',)
-    list_filter = ('titulo', 'estadistica_list',)
+    list_display = ('titulo',)
+    search_fields = ('titulo',)
+    list_filter = ('titulo',)
 
 @admin.register(Grafico)
 class GraficoAdmin(admin.ModelAdmin):
